2b797ddf5b8ecf38d6009a2ff8f3d49a3f403130
codew <codewithshakil@gmail.com>
2026-02-09 12:13:55 +0600

    feat: reduce splash screen delay from 5 to 3 seconds

--- lib/views/screen/Splash/splash_screen.dart
+++ lib/views/screen/Splash/splash_screen.dart
@@ -8,7 +8,7 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Get.find<SplashController>().jumpNextScreen();
     });
 
